5a5240eb938d210246037389146835a9910a5cb9
brouxhaha <andy.debroux@gmail.com>
2018-06-15 08:26:35 -0700

    Vue-Resource: update for reusable functions for get and post request

--- Vue-Resource/src/App.vue
+++ Vue-Resource/src/App.vue
@@ -13,6 +13,8 @@
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
                 <hr>
+                <input type="text" class="form-control" v-model="node">
+                <br><br>
                 <button class="btn btn-primary" @click="fetchData">Get Data</button>
                 <br><br>
                 <ul class="list-group">
@@ -31,21 +33,37 @@
                     username: '',
                     email: ''
                 },
-                users: []
-
+                users: [],
+                resource: {},
+                node: 'data'
             };
         },
         methods: {
             submit() {
-                this.$http.post('https://vuejs-http-fde9b.firebaseio.com/data.json', this.user)
+                /*this.$http.post('data.json', this.user)
                     .then(response => {
                         console.log(response);
                     }, error => {
                         console.log(error);
-                    });
+                    });*/
+
+                //this.resource.save({}, this.user);
+                this.resource.saveAlt(this.user);
             },
             fetchData() {
-                this.$http.get('https://vuejs-http-fde9b.firebaseio.com/data.json')
+                /*this.$http.get('data.json')
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        const resultArray = [];
+                        for (let key in data){
+                            resultArray.push(data[key]);
+                        }
+                        this.users = resultArray;
+                    });*/
+
+                this.resource.getData({node: this.node})
                     .then(response => {
                         return response.json();
                     })
@@ -57,6 +75,19 @@
                         this.users = resultArray;
                     });
             }
+        },
+        created() {
+            const customActions = {
+                saveAlt: {
+                    method: 'POST',
+                    url: 'alternative.json'
+                },
+                getData: {
+                    method: 'GET'
+                }
+            };
+
+            this.resource = this.$resource('{node}.json', {}, customActions);
         }
     }
 </script>
--- Vue-Resource/src/main.js
+++ Vue-Resource/src/main.js
@@ -4,6 +4,22 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
+Vue.http.options.root = 'https://vuejs-http-fde9b.firebaseio.com';
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  // POST creates new data
+  if(request.method == 'POST'){
+    // PUT overrides data
+    request.method = 'PUT';
+  }
+  next(response => {
+    // change the json method to return an object with a key because the PUT above changes it to non-object 
+    response.json = () => {
+      return {messages: response.body};
+    }
+  });
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
