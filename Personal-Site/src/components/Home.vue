e73b86200aedb77fc1a986004e082b682d3128ab
brouxhaha <andy.debroux@gmail.com>
2018-06-27 21:11:37 -0700

    personal-site: continue working on work section - added grid and project styles; added project data and v-for to loop through

--- Personal-Site/src/components/Home.vue
+++ Personal-Site/src/components/Home.vue
@@ -1,14 +1,25 @@
 <template>
     <div class="panel panel--odd">
         <h2 class="panel__heading">Work</h2>
-        <div class="wrapper work grid">
-            <div class="grid__split">
-                <p>My work is a combination of design and development. Outside a few cases, I have designed every site I have developed. I focus on creating user-friendly designs that aim to accomplish the client’s goals. </p>
-                <p>I utilize the latest HTML and CSS while adding JavaScript for interactivity and functionality. When possible, I used the latest, greatest CSS with fallbacks for browsers that need them.</p>
-            </div>
-            <div class="grid__split">
+        <div class="wrapper">
+            <div class="work grid--work">
+                <div class="work__text">
+                    <p>My work is a combination of design and development. Outside a few cases, I have designed every site I have developed. I focus on creating user-friendly designs that aim to accomplish the client’s goals. </p>
+                    <p>I utilize the latest HTML and CSS while adding JavaScript for interactivity and functionality. When possible, I used the latest, greatest CSS with fallbacks for browsers that need them.</p>
+                </div>
                 <div class="project--main">
-                    <img src="../assets/travel-alaska_initial_2000x1200.jpg">
+                    <div class="project--main__image">
+                        <img src="../assets/travel-alaska_initial_2000x1200.jpg">
+                    </div>
+                </div>
+
+                <div class="projects grid--projects">
+                    <div class="project__item" v-for="(project, key, index) in projects" v-if="index !== 0">
+                        <img :src="project.homeImg" class="project__img">
+                        <h3 class="project__name">
+                            {{ project.name }}
+                        </h3>
+                    </div>
                 </div>
             </div>
         </div>
@@ -17,13 +28,39 @@
 </template>
 
 <script>
-    /*import Projects from './components/projects/Projects.vue'
+    /*import Projects from './components/projects/Projects.vue'*/
 
     export default {
-        components: {
-            Projects: Projects
+        data() {
+            return {
+                projects: {
+                    TravelAlaska: {
+                        name: 'State of Alaska & Alaska Travel Industry Association',
+                        description: '',
+                        homeImg: '/src/assets/travel-alaska_initial_2000x1200.jpg'
+                    },
+                    AnchorageLibrary: {
+                        name: 'Anchorage Public Library',
+                        description: '',
+                        homeImg: '/src/assets/anchorage-library_initial_2000x1200.jpg'
+                    },
+                    VisitKetchikan: {
+                        name: 'City of Ketchikan',
+                        description: '',
+                        homeImg: '/src/assets/visit-ketchikan_initial_2000x1200.jpg'
+                    },
+                    AnchorageMuseum: {
+                        name: 'Anchorage Museum',
+                        description: '',
+                        homeImg: '/src/assets/anchorage-museum_initial_2000x1200.jpg'
+                    }
+                }
+            }
         }
-    }*/
+        /*components: {
+            Projects: Projects
+        }*/
+    }
 </script>
 
 <style lang="sass">
@@ -89,10 +126,24 @@ background: linear-gradient(90deg, rgba(255,150,0,1) 0%, rgba(255,150,0,1) 60%, 
         grid-gap: 100px;
     }
 
+    .grid--work {
+        display: grid;
+        grid-gap: 100px 50px;
+        align-items: center;
+    }
+
+    .work__text {
+        grid-column: 1 / 4;
+    }
+
     .project--main {
-        padding-left: 15px;
+        grid-column: 4 / 7;
+    }
+
+    .project--main__image {
         position: relative;
-        
+        padding-left: 15px;
+
         &:after {
             content: '';
             width: 3px;
@@ -103,5 +154,70 @@ background: linear-gradient(90deg, rgba(255,150,0,1) 0%, rgba(255,150,0,1) 60%, 
             transform: translateY(-50%);
             background: var(--main-warm);
         }
+    }
+
+    .grid--projects {
+        display: grid;
+        grid-column: 1 / 7;
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 0;
+    }
+
+    .project__item {
+        box-shadow: 5px 0 3px 0 rgba($main-cool, 0.5);
+        position: relative;
+
+        &:before {
+            content: '';
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            background: linear-gradient(180deg, rgba($main-cool, 0) 0%, rgba($main-cool, 1) 100%);
+        }
+
+        &:first-child {
+            z-index: 2;
+        }
+
+        &:nth-child(2) {
+            z-index: 1;
+        }
+
+        &:nth-child(3) {
+            z-index: 0;
+        }
+    }
+
+    .project__name {
+        font-size: 1.25rem;
+        line-height: 1;
+        text-align: center;
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        width: 100%;
+        margin: 0;
+        text-shadow: 0 1px 6px rgba($black, 0.5);
+
+        &:after {
+            content: '';
+            height: 3px;
+            width: 100px;
+            position: absolute;
+            bottom: -10px;
+            background: var(--main-warm);
+            left: 50%;
+            transform: translateX(-50%);
+        }
+    }
+    
+    .project__img {
+        display: block;
+    }
+
+    .spacing {
+        margin-bottom: 5rem;
     }
 </style>
